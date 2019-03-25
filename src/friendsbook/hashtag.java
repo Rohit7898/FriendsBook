d41c205e07973d3058e853d0c8330f762e153298
Rohit7898 <rprajapati688@gmail.com>
2019-03-24 19:51:58 -0600

    pre final

--- src/friendsbook/hashtag.java
+++ src/friendsbook/hashtag.java
@@ -5,6 +5,7 @@
  */
 package friendsbook;
 
+import static friendsbook.User.uid;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -23,6 +24,7 @@ public class hashtag {
         User u=new User();
         final String DB_URL="jdbc:mysql://mis-sql.uhcl.edu/prajapatir1738";
         Connection conn = null;
+        ArrayList <String> friends =new ArrayList<String>();
         Scanner in=new Scanner(System.in);
         Statement statement = null;
         ResultSet resultSet = null;
@@ -38,30 +40,52 @@ public class hashtag {
             statement2 = conn.createStatement();
             resultSet = statement.executeQuery("Select * from hashtag order by count desc limit 3");
             resultSet1 = statement1.executeQuery("Select * from post where type='P'");
-            int seq=0;
+            int seq=0,se=0;
+            boolean empty=true;
             while(resultSet.next())
             {
                 seq++;
                 System.out.println(seq+". "+resultSet.getString(1));
-            } 
-            System.out.println("Enter the tag you want to explore");
-            String tag=in.next();
-            
-            while(resultSet1.next())
-            {
-                if(resultSet1.getString(3).contains("tag")&&u.friends.contains(resultSet1.getString(2)))
-                {
-                    resultSet2 = statement2.executeQuery("Select content from post Where pid ='"+resultSet1.getString(1)+"'");
-                    seq++;
-                }
-                if(resultSet2!=null)
-                {
-                   while(resultSet2.next())
-                    {
-                        System.out.println(seq+". "+resultSet2.getString(1));
-                    }  
-                } 
+                empty=false;
             }
+            if(empty)
+            {
+                System.out.println("No hashtag in trend");
+            }
+            System.out.println("Enter the tag you want to explore or press 'x' to go back");
+            String tag=in.next();
+            if(tag.equals("x"))
+            {
+                User.Logedin();
+            }
+            else
+            {
+                resultSet=statement.executeQuery("Select * from friends Where id1='"+uid+"' or id2='"+uid+"'");
+                while(resultSet.next()){
+                    if(resultSet.getString(1).equals(uid)){
+                        friends.add(resultSet.getString(2));
+                    }
+                    else if(resultSet.getString(2).equals(uid)){
+                        friends.add(resultSet.getString(1));
+                    }
+                }
+                while(resultSet1.next())
+                {
+                    if(resultSet1.getString(3).contains(tag)&&friends.contains(resultSet1.getString(2)))
+                    {
+                        resultSet2 = statement2.executeQuery("Select uid, content from post Where pid ='"+resultSet1.getString(1)+"'");
+                    }
+                    if(resultSet2!=null)
+                    {
+                       while(resultSet2.next())
+                        {
+                            se++;
+                            System.out.println(se+". "+resultSet2.getString(1)+": "+resultSet2.getString(2));
+                        }  
+                    } 
+                }
+            }
+            
             
         }
         catch(SQLException e)
